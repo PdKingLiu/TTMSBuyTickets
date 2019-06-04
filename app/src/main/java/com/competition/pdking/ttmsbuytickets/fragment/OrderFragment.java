432311bc94fa2f9e2e483afe8ceeb4e26d219337
PdKingLiu <931942280@qq.com>
2019-06-04 14:20:02 +0800

    初始化存储库和基本搭建项目

---
+++ app/src/main/java/com/competition/pdking/ttmsbuytickets/fragment/OrderFragment.java
@@ -0,0 +1,10 @@
+package com.competition.pdking.ttmsbuytickets.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * @author liupeidong
+ * Created on 2019/6/4 14:18
+ */
+public class OrderFragment extends Fragment {
+}
